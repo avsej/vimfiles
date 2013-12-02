1428a6035d81104ce5d7a94556a02c2e0dce503a
Sergey Avseyev <sergey.avseyev@gmail.com>
2013-12-02 14:18:07 +0300

    c++ template

---
+++ skel/template.cc
@@ -0,0 +1,20 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+
+    int
+main(int argc, char *argv[])
+{
+    return 0;
+}
